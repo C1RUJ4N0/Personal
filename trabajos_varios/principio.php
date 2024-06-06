8fd7d68cab53ba8532bc39648ff66d6f8f547783
CIRUJANO <juan.marquez@educacionadventista.org.ar>
2024-06-06 19:37:53 -0300

    Trabajos Varios

---
+++ trabajos_varios/principio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma de Elementos</title>
+</head>
+<body>
+    <?php
+    $num1 = 2;
+    $num2 = 3;
+
+    $suma = $num1 + $num2;
+    echo '<h1>la suma es: ' .$suma. '</h1>';
+    ?>
+</body>
+<?php
+$num1 = 1;
+$num2 = 3;
+
+$resta = $num1 - $num2;
+echo '<h1>la resta es: ' .$resta. '</h1>';
+?> 
+<?php
+$num1 = 1;
+$num2 = 3;
+
+$mult = $num1 * $num2;
+echo '<h1>la multiplicacion es: ' .$mult. '</h1>';
+?> 
+<?php
+$num1 = 4;
+$num2 = 2;
+
+$div = $num1 / $num2;
+echo '<h1>la divicion es: ' .$div. '</h1>';
+?> 
+</html>
